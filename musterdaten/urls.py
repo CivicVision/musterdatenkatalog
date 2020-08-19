aeb9af070af0eee6886659e3e41a292445d05955
Mila Frerichs <mila.frerichs@gmail.com>
2020-08-19 09:16:33 +0200

    feat: add landingpage design
    add menus via templatetags

--- musterdaten/urls.py
+++ musterdaten/urls.py
@@ -4,5 +4,6 @@ from musterdaten import views
 
 app_name = 'musterdaten'
 urlpatterns = [
+    path('ueber/', views.UeberView.as_view(), name='ueber'),
     path('', views.IndexView.as_view(), name='index'),
 ]
