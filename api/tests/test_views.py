dbb3b7db4fdba8587a8c60454b54b82fdec3b493
Mila Frerichs <mila.frerichs@gmail.com>
2020-08-22 08:24:05 +0200

    fix: fix tests by using data
    instead of usiing args use kwarg data to pass to the get method and not
    the reverse url lookup method (looked up the docs)

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,9 +1,25 @@
+import json
 from api.test import APITestCase
+
+from musterdaten.tests.factories import (
+    ModeldatasetFactory,
+)
 
 class TestModeldataset(APITestCase):
     def test_ok(self):
         self.get('api:modeldataset-list', extra={'format': 'json'})
         self.assert_http_200_ok()
+
     def test_search_ok(self):
-        self.get('api:modeldataset-search', { 'q': "test" }, extra={'format': 'json'})
+        self.get('api:modeldataset-search', data={ 'q': "test" }, extra={'format': 'json'})
         self.assert_http_200_ok()
+
+    def test_search_result(self):
+        dataset = ModeldatasetFactory(title="San Diego")
+
+        response = self.get('api:modeldataset-search', data={ 'q': "ego" }, extra={'format': 'json'})
+        data = json.loads(response.content)
+
+        self.assert_http_200_ok()
+        assert len(data) == 1
+        assert data[0]["title"] == dataset.title
