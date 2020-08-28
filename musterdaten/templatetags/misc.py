26af07f8d23be42427b553ce3ba81e99ac009573
milafrerichs <mila.frerichs@gmail.com>
2020-08-28 09:46:24 +0200

    chore: small adjustments to the form

---
+++ musterdaten/templatetags/misc.py
@@ -0,0 +1,7 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def join_by_attr(the_list, attr_name, separator=', '):
+    return separator.join(getattr(i, attr_name) for i in the_list)
