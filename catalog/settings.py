2162d6b942b4d0b4a76102f6071076e66fdef0ee
milafrerichs <mila.frerichs@gmail.com>
2020-10-19 14:27:07 +0200

    feat: change heroku release tasks
    include tailwind build in post_compile to build the css for production
    add whitenoise

--- catalog/settings.py
+++ catalog/settings.py
@@ -38,6 +38,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'catalog.urls'
