afacaa8e7ec0a45cba22003ff04fba9f05a3f14c
milafrerichs <mila.frerichs@gmail.com>
2020-12-01 12:55:12 +0100

    feat: get demo to work as expected

--- crowdsourcing/tests/test_views.py
+++ crowdsourcing/tests/test_views.py
@@ -8,19 +8,3 @@ class TestIndex(TestCase):
     def test_ok(self):
         self.get_check_200("crowdsourcing:index")
 
-class TestEvaluate(TestCase):
-    def test_ok(self):
-        dataset = DatasetFactory()
-        modeldataset = ModeldatasetFactory()
-        data = {"dataset": str(dataset.id), "modeldataset": str(modeldataset.id) }
-
-        response = self.post('crowdsourcing:evaluate', data=data)
-        self.response_302(response)
-
-    def test_failure(self):
-        dataset = DatasetFactory()
-        modeldataset = ModeldatasetFactory()
-        data = {"dataset": "stri", "modeldataset": str(modeldataset.id) }
-
-        response = self.post('crowdsourcing:evaluate', data=data)
-        self.response_400(response)
