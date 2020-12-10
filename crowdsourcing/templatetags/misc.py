6752cb28cd9ed8413051a363bb452f8564da6b39
milafrerichs <mila.frerichs@gmail.com>
2020-12-10 12:39:57 +0100

    chore: update step on sidebar; adjust text sizes

--- crowdsourcing/templatetags/misc.py
+++ crowdsourcing/templatetags/misc.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter
 def join_by_attr(the_list, attr_name, separator=', '):
     return separator.join(getattr(i, attr_name) for i in the_list)
+
+@register.simple_tag
+def setvar(val=None):
+    return val
