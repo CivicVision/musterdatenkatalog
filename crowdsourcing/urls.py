88f6df52ba6282bfc840fdec4f57e6ae7583e3a8
Mila Frerichs <mila.frerichs@gmail.com>
2021-01-08 14:42:21 +0100

    chore: add impressum and footer

--- crowdsourcing/urls.py
+++ crowdsourcing/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from crowdsourcing.views import IndexView, UeberView, EvaluateFormView, FORMS
+from crowdsourcing.views import IndexView, UeberView, ImpressumView, EvaluateFormView, FORMS
 
 app_name = 'crowdsourcing'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('ueber/', UeberView.as_view(), name='ueber'),
+    path('impressum/', ImpressumView.as_view(), name='impressum'),
     path('score/', EvaluateFormView.as_view(FORMS), name='evaluate'),
 ]
