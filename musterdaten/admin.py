37adc177ca39e5eb7ac5a07030bf1b9bdf9280b3
milafrerichs <mila.frerichs@gmail.com>
2021-01-11 10:39:45 +0100

    feat: IP anonymization using yacryptopan
    don't use the real IP for a user but use a tool to anonymize the IP.
    we don't care about the IP we just want to know if you were here before
    and show you different datasets

--- musterdaten/admin.py
+++ musterdaten/admin.py
@@ -1,21 +1,33 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 
-from musterdaten.models import Score, Dataset, City, Category, License, State, Modeldataset, Modelsubject, Leika, Top3
+from musterdaten.models import Score, Dataset, City, Category, License, State, Modeldataset, Modelsubject, Leika, Top3, CustomUser
 
 class Top3tInline(admin.TabularInline):
     model = Top3
     extra = 3
 
 
+class ScoreInlineAdmin(admin.StackedInline):
+    model = Score
+
+
 class DatasetAdmin(admin.ModelAdmin):
     inlines = (Top3tInline,)
+
 
 class ModeldatasetInline(admin.StackedInline):
     model = Modeldataset
 
+
 class ModelsubjectAdmin(admin.ModelAdmin):
     inlines = (ModeldatasetInline,)
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+     model = CustomUser
+     list_display = ("pk", "ip_address")
+     inlines = (ScoreInlineAdmin, )
 
 
 admin.site.unregister(Group)
@@ -29,4 +41,4 @@ admin.site.register(Modeldataset)
 admin.site.register(State)
 admin.site.register(Modelsubject, ModelsubjectAdmin)
 admin.site.register(Leika)
-
+admin.site.register(CustomUser, CustomUserAdmin)
