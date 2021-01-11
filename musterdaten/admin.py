8aa117b576fc9452490942301614e6c1c1d0b3f5
milafrerichs <mila.frerichs@gmail.com>
2021-01-11 09:55:17 +0100

    feat: add more ways to use the admin
    add filters, search and more models to the admin interface

--- musterdaten/admin.py
+++ musterdaten/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 
-from musterdaten.models import Score, Dataset, City, Category, License, State, Modeldataset, Modelsubject, Leika, Top3
+from musterdaten.models import (
+    Score,
+    Dataset,
+    City,
+    Category,
+    License,
+    State,
+    Modeldataset,
+    Modelsubject,
+    Leika,
+    Top3,
+    NoMatchScore,
+)
 
 class Top3tInline(admin.TabularInline):
     model = Top3
@@ -9,23 +21,49 @@ class Top3tInline(admin.TabularInline):
 
 
 class DatasetAdmin(admin.ModelAdmin):
+    search_fields = ("title", )
     inlines = (Top3tInline,)
+    list_filter = ("modeldataset__modelsubject", "modeldataset",)
+
 
 class ModeldatasetInline(admin.StackedInline):
     model = Modeldataset
+
 
 class ModelsubjectAdmin(admin.ModelAdmin):
     inlines = (ModeldatasetInline,)
 
 
+class ScoreInlineAdmin(admin.StackedInline):
+     model = Score
+
+
+class ModeldatasetAdmin(admin.ModelAdmin):
+    model = Modeldataset
+    list_filter = ("modelsubject", )
+    search_fields = ("title", )
+    inlines = (ScoreInlineAdmin, )
+
+
+class ScoreAdmin(admin.ModelAdmin):
+    model = Score
+    list_display = ("dataset", "modeldataset", "session_id")
+
+
+class NoMatchScoreAdmin(admin.ModelAdmin):
+    model = NoMatchScore
+    list_display = ("dataset", "session_id", "topic", "term")
+
+
 admin.site.unregister(Group)
-admin.site.register(Score)
+admin.site.register(Score, ScoreAdmin)
+admin.site.register(NoMatchScore, NoMatchScoreAdmin)
 admin.site.register(Dataset, DatasetAdmin)
 
 admin.site.register(City)
 admin.site.register(Category)
 admin.site.register(License)
-admin.site.register(Modeldataset)
+admin.site.register(Modeldataset, ModeldatasetAdmin)
 admin.site.register(State)
 admin.site.register(Modelsubject, ModelsubjectAdmin)
 admin.site.register(Leika)
