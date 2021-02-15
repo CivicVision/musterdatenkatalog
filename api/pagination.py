943f8b4695385bd1f8c3eb48f8ef230dfde0e604
Mila Frerichs <mila.frerichs@gmail.com>
2021-02-15 13:32:44 +0100

    enable per_page query param
    to get all of the modeldatasets we want to change the per_page query param
    we need to enable it first for it to work
    and now we need to remove the page_size entry in the response because it would be not correct if the query param is set. no need to add it anyway.

--- api/pagination.py
+++ api/pagination.py
@@ -5,11 +5,13 @@ from catalog import settings
 
 
 class CustomPagination(pagination.PageNumberPagination):
+    
+    page_size_query_param = 'per_page'
+    
     def get_paginated_response(self, data):
         return Response({
             'page': self.page.number,
             'next_page': self.get_next_link(),
             'previous_page': self.get_previous_link(),
-            'per_page': settings.REST_FRAMEWORK.get("PAGE_SIZE"),
             'data': data
         })
